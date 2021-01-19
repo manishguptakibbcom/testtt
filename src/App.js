3a03b952e3465f8c36875c46be8263825eb1b5f9
Manish Gupta <manish.gupta@kibbcom.in>
2021-01-19 21:48:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  async function connect2QB(e) {
+    // try{
+    //   const response = await fetch("");
+    //   console.log(response.text());
+    //   const data = await response.text();
+    //   console.log(data)
+    //   //https://jsonplaceholder.typicode.com/todos/1
+    //   console.log('The link was clicked.');
+    // }catch(e){
+    //   console.log(e);
+    // }
+    fetch("")
+        .then(response => response.text())
+        .then((response) => {
+            console.log(response)
+        })
+        .catch(err => console.log(err)) 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connect2QB}>
+        Activate Lasers
+      </button>
     </div>
   );
 }
